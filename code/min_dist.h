c11ff2fe1a5455b22099b243d0b94eb7253d13b1
Vercasson Victor <vercasson1605@gmail.com>
2023-04-30 19:12:49 +0200

    lol ^^

--- code/min_dist.h
+++ code/min_dist.h
@@ -1,6 +1,10 @@
 #include <limits.h>
-#include "abs-cor.h"
+#include "abs.h"
 
-/*@ ensures \forall integer i; 0 <= i < n ==> (\forall integer j; i < j < n ==> \result <= abs(tab[i]-tab[j]));
+/*@ 
+    @ requires n > 0;
+    @ requires \valid(tab + (0..n-1));
+    @ assigns \nothing;
+    @ ensures \forall integer i; 0 <= i < n ==> (\forall integer j; i < j < n ==> \result <= abs(tab[i]-tab[j]));
  */
 int min_dist(int *tab, unsigned int n);
