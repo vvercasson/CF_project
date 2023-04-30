706bb6a885c5d9833c4976402bb7fdfc30ac6c81
Vercasson Victor <vercasson1605@gmail.com>
2023-04-30 18:19:42 +0200

    pas encore juste, rte de max-min >= INTMAX passe pas

--- code/max_dist.c
+++ code/max_dist.c
@@ -5,6 +5,20 @@ int max_dist(int *tab, unsigned int n)
     int min = tab[0];
     int max = tab[0];
     unsigned int i = 1;
+    /*@
+        @ loop assigns i, min, max;
+
+        @ loop invariant 1 <= i <= n;
+        
+        @ loop invariant \forall integer a,b; (1 <= a <= b < i) ==> (max - min) >= (abs(tab[a] - tab[b]));
+
+        @ loop invariant \forall integer a; (1 <= a < i) ==> min <= tab[a] <= max;
+
+        @ loop invariant \exists integer a; (1 <= a < i)  ==> (tab[a] == min);
+        @ loop invariant \exists integer a; (1 <= a < i)  ==> (tab[a] == max);
+
+        @ loop variant n - i;
+    */
     while (i < n)
     {
         if (tab[i] < min)
--- code/max_dist.h
+++ code/max_dist.h
@@ -1,6 +1,12 @@
 #include <limits.h>
 #include "abs.h"
 
-/*@ ensures \forall integer a,b; 0 <= a < b < n ==> \result >= abs(tab[a]-tab[b]);
+/*@ 
+    @ requires \valid(tab+(0..n-1));
+    @ requires n >= 1;
+    @ terminates \true;
+    @ assigns \nothing;
+    @ ensures \result >= 0;
+    @ ensures \forall integer a,b; 0 <= a < b < n ==> \result >= abs(tab[a]-tab[b]);
  */
 int max_dist(int *tab, unsigned int n);
