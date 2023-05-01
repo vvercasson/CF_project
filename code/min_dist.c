e94066d0e14e9b7f4dd9458e03fc3e2b210e0765
Vercasson Victor <vercasson1605@gmail.com>
2023-05-02 00:21:39 +0200

    ajout commentaire max dist ajout variant / invariant min dist

--- code/min_dist.c
+++ code/min_dist.c
@@ -3,10 +3,20 @@ int min_dist(int *tab, unsigned int n)
 {
     int min = abs(tab[0] - tab[1]);
     unsigned int i = 0;
+    /*@
+        @ loop assigns j, min_i;
+        @ loop invariant 0 <= i <= n - 1;
+        @ loop variant (n - 1)-i;
+    */
     while (i < n - 1)
     {
         int min_i = abs(tab[i] - tab[i + 1]);
         unsigned int j = i + 2;
+        /*@
+            @ loop assigns d, min_i, j;
+            @ loop invariant i + 2 <= j <= n;
+            @ loop variant n - j;
+        */
         while (j < n)
         {
             int d = abs(tab[i] - tab[j]);
